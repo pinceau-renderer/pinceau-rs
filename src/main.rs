9cba8fadf482bc4d69319047aef5680c71a6d5ed
melvi-l <melvil.miglio@gmail.com>
2025-02-26 17:07:47 +0100

    add state

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 use math::vec3::Vec3;
 use math::mat4::Mat4;
+use pinceau_rs::run;
+
 
 mod math {
     pub mod utils;
@@ -9,11 +11,9 @@ mod math {
     pub mod mat4;
 }
 
+
 fn main() {
-    let vec = Vec3::<f32>::new(1., 0., 0.);
-    println!("{}", vec);
-    let mat = Mat4::<f32>::identity();
-    println!("{}", mat);
-    let projection = Mat4::perspective(45., 1.5, 0.1, 1000.);
-    println!("{}", projection);
+    pollster::block_on(run());
 }
+
+
---
+++ src/state.rs
@@ -0,0 +1,93 @@
+use winit::{event::WindowEvent, window::Window};
+
+pub struct State<'a> {
+    pub surface: wgpu::Surface<'a>,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub config: wgpu::SurfaceConfiguration,
+    pub size: winit::dpi::PhysicalSize<u32>,
+    pub window: &'a Window,
+}
+
+impl<'a> State<'a> {
+    pub async fn new(window: &'a Window) -> State<'a> {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::default(),
+                    label: Some("Pinceau device"),
+                    memory_hints: Default::default(),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window,
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
